f1376df61b136e95f82959d936ac9d8ef469aa25
JCharly <carlosepintor@hotmail.com>
2018-09-26 23:44:05 -0300

    Add Home view

--- client/HelloWorld.js
+++ client/HelloWorld.js
@@ -1,12 +1,38 @@
 import React from 'react'
+import MainRouter from './MainRouter'
+import { BrowserRouter } from 'react-router-dom'
 import { hot } from 'react-hot-loader'
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+import { indigo, pink } from 'material-ui/colors'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+    light: '#757de8',
+    main: '#3f51b5',
+    dark: '#002984',
+    contrastText: '#fff',
+  },
+  secondary: {
+    light: '#ff79b0',
+    main: '#ff4081',
+    dark: '#c60055',
+    contrastText: '#000',
+  },
+    openTitle: indigo['400'],
+    protectedTitle: pink['400'],
+    type: 'light'
+  }
+})
 
 class HelloWorld extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Hello World everyone!!</h2>
-      </div>
+      <BrowserRouter>
+        <MuiThemeProvider theme={theme}>
+          <MainRouter/>
+        </MuiThemeProvider>
+      </BrowserRouter>
     )
   }
 }
--- webpack.config.client.js
+++ webpack.config.client.js
@@ -20,6 +20,9 @@ const config = {
       test: /\.jsx?$/,
       exclude: /node_modules/,
       use: ["babel-loader"]
+    },{
+      test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+      use: 'file-loader'
     }]
   },
   plugins: [
